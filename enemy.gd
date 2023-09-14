8286cd7a58cf2cc52504a828b6b6a5aa785ad26c
kpw6 <kpw6@njit.edu>
2023-09-14 18:37:00 -0400

    added enemy and better movement

--- enemy.gd
+++ enemy.gd
@@ -1,11 +1,25 @@
-extends RigidBody2D
+extends CharacterBody2D
 
-
+var speed = 5
+var playerchase = false
+var player = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
 	$AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
 
+func _physics_process(_delta):
+	if(playerchase):
+		position += (player.position - position)/speed
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+	
+	
+func _on_area_2d_body_entered(body):
+	player = body
+	playerchase = true
+
+func _on_area_2d_body_exited(_body):
+	player = null
+	playerchase = false
--- playermovement.gd
+++ playermovement.gd
@@ -3,22 +3,43 @@ extends CharacterBody2D
 
 @onready var _animation_player = $AnimatedSprite2D
 @export var speed = 50
+var last_direction
+
+func _ready():
+	_animation_player.play("idle_down")
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
-
-func _process(_delta):
-	if Input.is_action_pressed("ui_right"):
-		_animation_player.play("right")
-	elif Input.is_action_pressed("ui_left"):
-		_animation_player.play("left")
-	elif Input.is_action_pressed("ui_down"):
-		_animation_player.play("down")
-	elif Input.is_action_pressed("ui_up"):
-		_animation_player.play("up")
-	else:
-		_animation_player.stop()
+	player_animate (input_direction)
+func player_animate(direction):
+	match(direction):
+		Vector2(1,0):
+			if (velocity != Vector2(0,0)):
+				_animation_player.play("right")
+				last_direction = direction
+		Vector2(-1,0):
+			if (velocity != Vector2(0,0)):
+				_animation_player.play("left")
+				last_direction = direction
+		Vector2(0,-1):
+			if (velocity != Vector2(0,0)):
+				_animation_player.play("up")
+				last_direction = direction
+		Vector2(0,1):
+			if (velocity != Vector2(0,0)):
+				_animation_player.play("down")
+				last_direction = direction
+	if(!velocity):
+		match(last_direction):
+			Vector2(1,0):
+				_animation_player.play("idle_right")
+			Vector2(-1,0):
+				_animation_player.play("idle_left")
+			Vector2(0,-1):
+				_animation_player.play("idle_up")
+			Vector2(0,1):
+				_animation_player.play("idle_down")
 func _physics_process(_delta):
 	get_input()
 	move_and_slide()
